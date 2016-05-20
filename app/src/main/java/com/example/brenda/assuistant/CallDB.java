d3675c79b1dcd7292c42554f75928c64bd39c652
sSsXzZz <shalinpatel75@gmail.com>
2016-05-20 10:00:43 -0400

    Created java function to create new entry in activity table

--- app/src/main/java/com/example/brenda/assuistant/CallDB.java
+++ app/src/main/java/com/example/brenda/assuistant/CallDB.java
@@ -13,11 +13,11 @@ import javax.net.ssl.HttpsURLConnection;
 
 
 public class CallDB {
-    static String  url_address = "http://b343c848.ngrok.io/";
+    static String  url_address = "http://25989377.ngrok.io/";
     public static void main(String[] args) {
-        updateTable_log(4,"GOOG,4;","World ELLO!");
+        insertTable_activity("Shalin","Apple sucks!");
+        //updateTable_log(4,"GOOG,4;","World ELLO!");
     }
-
 
     public static void createMeetings(){
         insertTable_log(1,
@@ -88,7 +88,7 @@ public class CallDB {
     public static String getTable(String table){
         String table_data = "";
         try {
-            URL url = new URL("http://b343c848.ngrok.io/" + table);
+            URL url = new URL( url_address + table);
             BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
             table_data = br.readLine();
         } catch (Exception ex) {
@@ -107,7 +107,7 @@ public class CallDB {
         }
         POST = POST + "&ticker_sentiment=" + ticker_sentiment + "&notes=" + notes;
         try {
-            URL url = new URL( "http://b343c848.ngrok.io/log_write");
+            URL url = new URL( url_address + "log_write");
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
             con.setDoOutput(true);
             con.setRequestMethod("POST");
@@ -147,7 +147,7 @@ public class CallDB {
     public static void updateTable_log(int id, String ticker_sentiment, String notes){
         String POST = "id=" + Integer.toString(id) + "&ticker_sentiment=" + ticker_sentiment + "&notes=" + notes + "&done=true";
         try {
-            URL url = new URL( "http://b343c848.ngrok.io/log_update");
+            URL url = new URL( url_address + "log_update");
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
             con.setDoOutput(true);
             con.setRequestMethod("POST");
@@ -184,4 +184,45 @@ public class CallDB {
             ex.printStackTrace();
         }
     }
+    public static void insertTable_activity(String employee, String content){
+        String POST = "employee=" + employee + "&content=" + content;
+        try {
+            URL url = new URL( url_address + "activity_write");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setDoOutput(true);
+            con.setRequestMethod("POST");
+            con.setRequestProperty("Accept-Charset","UTF-8");
+            con.setReadTimeout(10000);
+            con.setConnectTimeout(15000);
+            con.connect();
+
+            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+            wr.writeBytes( "activity_write?" + POST );
+            wr.flush();
+            wr.close();
+
+            int responseCode = con.getResponseCode();
+            System.out.println("POST Response Code :: " + responseCode);
+
+            if (responseCode == HttpURLConnection.HTTP_OK) { //success
+                BufferedReader in = new BufferedReader(new InputStreamReader(
+                        con.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                // print result
+                System.out.println(response.toString());
+            } else {
+                System.out.println("POST request not worked");
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
 }
