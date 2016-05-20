a49c0dd9e2fa81fee6385e7f5a555f3a33748fb2
ysharma1126 <ysharma1126@gmail.com>
2016-05-19 22:17:22 -0400

    Create TestObject2.java

---
+++ app/src/main/java/com/example/brenda/assuistant/TestObject2.java
@@ -0,0 +1,25 @@
+package com.example.brenda.assuistant;
+
+
+public class TestObject2 {
+
+
+    private String user;
+    private String content;
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
