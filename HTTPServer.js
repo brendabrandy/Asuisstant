1b7ced829f42fec20377bfc70ca2b88c245bc142
sSsXzZz <shalinpatel75@gmail.com>
2016-05-20 04:08:39 -0400

    Added GET request handling for employee & log tables  and POST request handling for log table

--- HTTPServer.js
+++ HTTPServer.js
@@ -1,11 +1,12 @@
 var dispatcher = require('httpdispatcher');
 var http = require('http');
 var pg = require('pg');
+var url = require('url');
+var SQL = require('j-sql');
 
-//Lets define a port we want to listen to
 const PORT=8080; 
+var conString = "postgres://postgres@localhost/Assuistant";
 
-//Lets use our dispatcher
 function handleRequest(request, response){
     try {
         //log the request on console
@@ -17,42 +18,94 @@ function handleRequest(request, response){
     }
 }
 
-//For all your static (js/css/images/etc.) set the directory name (relative path).
 dispatcher.setStatic('resources');
 
-//A sample GET request    
-dispatcher.onGet("/ticker",function(req,res){
-	var conString = "postgres://postgres@localhost/Assuistant";
+dispatcher.onGet("/ticks",function(req,res){
 	var client = new pg.Client(conString);
 	client.connect(function(err) {
-	  if(err) {
-		return console.error('could not connect to postgres', err);
-	  }
-	  client.query('SELECT * FROM public.ticks', function(err, result) {
 		if(err) {
-		  return console.error('error running query', err);
+			return console.error('could not connect to postgres', err);
 		}
-		res.writeHead(200, {'Content-Type': 'application/json'});
-		res.end(JSON.stringify(result));
-		client.end();
-	  });
+		client.query('SELECT * FROM public.ticks', function(err, result) {
+			if(err) {
+			return console.error('error running query', err);
+			}
+			res.writeHead(200, {'Content-Type': 'application/json'});
+			res.end(JSON.stringify(result));
+			client.end();
+		});
 	});
-    //res.writeHead(200, {'Content-Type': 'text/plain'});
-    //res.end('Page One');
 }); 
 
-//A sample POST request
-dispatcher.onPost("/post1", function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Got Post Data');
-});
+dispatcher.onGet("/log",function(req,res){
+	var client = new pg.Client(conString);
+	client.connect(function(err) {
+		if(err) {
+			return console.error('could not connect to postgres', err);
+		}
+		client.query('SELECT * FROM public.log', function(err, result) {
+			if(err) {
+			  return console.error('error running query', err);
+			}
+			res.writeHead(200, {'Content-Type': 'application/json'});
+			res.end(JSON.stringify(result));
+			client.end();
+		});
+	});
+}); 
 
+dispatcher.onGet("/employee",function(req,res){
+	var client = new pg.Client(conString);
+	client.connect(function(err) {
+		if(err) {
+			return console.error('could not connect to postgres', err);
+		}
+		client.query('SELECT * FROM public.employee', function(err, result) {
+			if(err) {
+			  return console.error('error running query', err);
+			}
+			res.writeHead(200, {'Content-Type': 'application/json'});
+			res.end(JSON.stringify(result));
+			client.end();
+		});
+	});
+}); 
+
+dispatcher.onPost("/test",function(req,res){
+	var querydata = url.parse(req.url, true).query;
+	var client = new pg.Client(conString);
+	querydata.id = parseInt(querydata.id);
+	var sql = SQL.insert('public.log').values(querydata).done();
+	console.log(sql);
+	var sql_script="";
+	for(var i = 0, len = sql.length; i<len;i++){
+		if(sql[i] == '\"' ){
+			sql_script = sql_script + '\'';
+		}
+		else{
+			sql_script=sql_script+sql[i];
+		}
+	}
+	client.connect(function(err) {
+		if(err) {
+			return console.error('could not connect to postgres', err);
+		}
+		client.query(sql_script, function(err, result) {
+			if(err) {
+				return console.error('error running query', err);
+			}
+			client.end();
+		});
+	});
+	res.writeHead(200, {'Content-Type': 'application/json'});
+	res.end(JSON.stringify(querydata));
+});
 
 //Create a server
 var server = http.createServer(handleRequest);
 
-//Lets start our server
+//start server
 server.listen(PORT, "0.0.0.0", function(){
-    //Callback triggered when server is successfully listening. Hurray!
+    //Callback triggered when server is successfully listening.
     console.log("Server listening on: http://localhost:%s", PORT);
 });
